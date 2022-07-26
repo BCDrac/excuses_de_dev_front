4fbf03f824eb492a198c4e570063045064e2a2c2
CecileB <59061296+BCDrac@users.noreply.github.com>
2022-07-26 11:07:22 +0200

    Fixed typos in urls

--- src/shared/constants/urls/urlBackend.js
+++ src/shared/constants/urls/urlBackend.js
@@ -1,4 +1,4 @@
 export const URL_BACK_GET_RANDOM_EXCUSE = '/excuse/getRandomExcuse'
-export const URL_BACK_GET_ALL_EXCUSES = '/getAllExcsues'
-export const URL_BACK_GET_ALL_EXCUSES_PAGEABLE = '/getAllExcusesPageable'
-export const URL_BACK_CREATE_EXCUSE = '/createExcuse';
+export const URL_BACK_GET_ALL_EXCUSES = '/excuse/getAllExcsues'
+export const URL_BACK_GET_ALL_EXCUSES_PAGEABLE = '/excuse/getAllExcusesPageable'
+export const URL_BACK_CREATE_EXCUSE = '/excuse/createExcuse';
